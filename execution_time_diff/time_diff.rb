8a0a3fa0cdc8aa2ec6eb5bef5be2bdb566872d39
App Academy Student <student@appacademy.io>
2017-04-07 09:55:04 -0700

    Implement benchmarking for two_sum and time_diff

--- execution_time_diff/time_diff.rb
+++ execution_time_diff/time_diff.rb
@@ -1,4 +1,65 @@
-require 'byebug'
+require 'io/console'
+require 'benchmark'
+
+def performance_test(size, count)
+  arrays_to_test = Array.new(count) { random_arr(size) }
+
+  Benchmark.benchmark(Benchmark::CAPTION, 9, Benchmark::FORMAT,
+                      "Avg. Slow:  ", "Avg. Faster: ") do |b|
+    slow_min = b.report("Tot. Min Slow:  ") { run_slow_min(arrays_to_test) }
+    faster_min = b.report("Tot. Min Faster: ") { run_fast_min(arrays_to_test) }
+    lcs_slow = b.report("Tot. LCS Slow: ") { run_slow_lcs(arrays_to_test) }
+    lcs_fast = b.report("Tot. LCS Faster: ") { run_fast_lcs(arrays_to_test) }
+    [slow_min/count, faster_min/count, lcs_slow/count, lcs_fast/count]
+  end
+end
+
+def self.random_arr(n)
+  Array.new(n) { rand(n) }
+end
+
+def run_slow_min(arrays)
+  arrays.each do |array|
+    array_to_test = array.dup
+    my_min_slow(array_to_test)
+  end
+end
+
+def run_fast_min(arrays)
+  arrays.each do |array|
+    array_to_test = array.dup
+    my_min_faster(array_to_test)
+  end
+end
+
+def run_slow_lcs(arrays)
+  arrays.each do |array|
+    array_to_test = array.dup
+    largest_contiguous_sum_slow(array_to_test)
+  end
+end
+
+def run_fast_lcs(arrays)
+  arrays.each do |array|
+    array_to_test = array.dup
+    largest_contiguous_sum_fast(array_to_test)
+  end
+end
+
+def self.wait_for_keypress(prompt)
+  puts prompt
+  STDIN.getch
+end
+
+def run_performance_tests(multiplier = 5, rounds = 3)
+  [1, 10, 100, 1000, 10000].each do |size|
+    size *= multiplier
+    wait_for_keypress(
+      "Press any key to benchmark sorts for #{size} elements"
+    )
+    performance_test(size, rounds)
+  end
+end
 
 def my_min_slow(arr)
   arr.each do |val1|
@@ -87,6 +148,8 @@ p largest_contiguous_sum_fast(list) == 8
 list = [-5, -1, -3]
 p largest_contiguous_sum_fast(list) == -1
 list = [-1, 2, 2, 2, -100, -1, 2, 2, 2, -4, -1, 5, 2, -10]
-p largest_contiguous_sum_fast(list)# == 8
+p largest_contiguous_sum_fast(list) == 8
 list = [-6, 5, 8, 7, -5, -7, 15]
 p largest_contiguous_sum_fast(list) == 23
+
+run_performance_tests(1)
--- two_sum/two_sum.rb
+++ two_sum/two_sum.rb
@@ -1,3 +1,58 @@
+require 'io/console'
+require 'benchmark'
+
+def performance_test(size, count)
+  arrays_to_test = Array.new(count) { random_arr(size) }
+
+  Benchmark.benchmark(Benchmark::CAPTION, 9, Benchmark::FORMAT,
+                      "Avg. Bad:  ", "Avg. Okay: ", "Avg. Best") do |b|
+    bad_sum = b.report("Tot. Bad:  ") { run_bad_sum(arrays_to_test) }
+    okay_sum = b.report("Tot. Okay: ") { run_okay_sum(arrays_to_test) }
+    best_sum = b.report("Tot. Best: ") { run_best_sum(arrays_to_test) }
+    [bad_sum/count, okay_sum/count, best_sum/count]
+  end
+end
+
+def self.random_arr(n)
+  Array.new(n) { rand(n) }
+end
+
+def run_bad_sum(arrays)
+  arrays.each do |array|
+    array_to_test = array.dup
+    bad_two_sum?(array_to_test, 20)
+  end
+end
+
+def run_okay_sum(arrays)
+  arrays.each do |array|
+    array_to_test = array.dup
+    okay_two_sum?(array_to_test, 62)
+  end
+end
+
+def run_best_sum(arrays)
+  arrays.each do |array|
+    array_to_test = array.dup
+    best_two_sum?(array_to_test, 70)
+  end
+end
+
+def self.wait_for_keypress(prompt)
+  puts prompt
+  STDIN.getch
+end
+
+def run_performance_tests(multiplier = 5, rounds = 3)
+  [1, 10, 100, 1000, 10000].each do |size|
+    size *= multiplier
+    wait_for_keypress(
+      "Press any key to benchmark sorts for #{size} elements"
+    )
+    performance_test(size, rounds)
+  end
+end
+
 def bad_two_sum?(arr, target_sum)
   arr.each_with_index do |val1, idx1|
     arr.each_with_index do |val2, idx2|
@@ -56,3 +111,5 @@ p okay_two_sum?(arr, 6) == true
 p okay_two_sum?(arr, 10) == false
 p best_two_sum?(arr, 6) == true
 p best_two_sum?(arr, 10) == false
+
+run_performance_tests(1)
